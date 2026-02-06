16cdd7ac2ebb3b2bcfbe7732001438580990a109
ausha <aushaavani@gmail.com>
2026-02-06 04:36:59 +0000

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StreamLine</title>
+    <style>
+        body {
+            background-color: white;
+            font-family: Arial;
+            padding: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to Streamline - v1</h1>
+    <p><b>Server IP:</b>
+        <?php echo $_SERVER['SERVER_ADDR']; ?>
+    </p>
+</body>
+</html>
